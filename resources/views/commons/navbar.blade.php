9ca3ec23c7d43099af71371f6b671c67482a5836
kaorin.ka <meisha.kaorin@gmail.com>
2020-07-29 03:46:33 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,3 +1,15 @@
+
+{{-- コメントです --}}
+{{--  .blade.php の拡張子のついたファイルでは、コメントは　この波かっこの中に書くこと  --}}
+{{-- bladeでは この波かっこを使用してコメントを書いてください --}}
+ {{--  --}}
+ {{--  --}}
+
+
+{{-- ユーザー登録ページへのリンクと　ログインページへのリンクに　差し替えた --}}
+{{-- OK --}}
+
+
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
@@ -11,10 +23,26 @@
             <ul class="navbar-nav mr-auto"></ul>
             {{--1つ目の   ul  に mr-auto クラスを設定して内容は空っぽにしておくと、2つ目の  ul  に追加した  li  の内容はナビゲーションバーの右側に表示されます。--}}    
 
+            
+            
+            <!--<ul class="navbar-nav">-->
+            <!--    {{-- タスク作成ページへのリンク --}}-->
+            <!--    <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>-->
+            <!--</ul>-->
+            
+            
+          
+            
             <ul class="navbar-nav">
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
+            
+        
+            
+            
         </div>
     </nav>
 </header>
