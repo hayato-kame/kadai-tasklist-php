7a0d768cb06a4ac77de0f65e37b085b423fb7fbf
kaorin.ka <meisha.kaorin@gmail.com>
2020-07-29 06:28:12 +0000

    web.php add

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,19 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+
+// 上に戻しました。
+// これはコメントにして
+// Route::get('/', 'TasksController@index');
+
+
+
+// これはそのままでいいのだろうか？ とりあえずこのまま
 Route::resource('tasks', 'TasksController');
-
 
 
 
@@ -23,3 +32,12 @@ Route::resource('tasks', 'TasksController');
 // ->name() はこのルーティングに名前をつけているだけです。後ほど、 Formやlink_to_route() で使用することになります。
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+
+
+// 認証は、LoginControllerが担当します。以下の3つのルーティングを routes/web.php に追記してください。
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
